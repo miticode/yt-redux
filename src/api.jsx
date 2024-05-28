76c87286602a63878205f80843927ca36320b686
Le Huynh Minh Tri <lehuynhminhtri09082003@gmail.com>
2024-05-28 10:16:39 +0700

    jhjh

--- src/api.jsx
+++ src/api.jsx
@@ -3,7 +3,7 @@ console.log(  process.env.PP_YT_API_KEY)
 const request = axios.create({
     baseURL: "https://youtube.googleapis.com/youtube/v3/",
     params:{
-        key: "AIzaSyAUtAHRc1lDArHR4vewrqeIjDDLnNH8Nbc",
+        key: "AIzaSyDpTqV6KdNOennSY-FMbgGVQZrayBIzrVs",
     },
 })
 export default request
